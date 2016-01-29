8d857bedb9b16f3940f6ee0c181bad1ff81c8838
SeJasonWang <52870104@qq.com>
2016-01-29 16:06:21 +0800

    Initial Commit

---
+++ SCURLManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCURLManager
+//
+//  Created by sichenwang on 16/1/29.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCURLManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCURLManager
+//
+//  Created by sichenwang on 16/1/29.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
